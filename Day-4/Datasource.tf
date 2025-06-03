a430199cfb318af0286a78291fc3047f8d1f2dc2
sanjayyadav <palleboinasanjaykumar@gmail.com>
2025-06-03 11:38:08 +0530

    Datasource.tf

--- Day-4/Datasource.tf
+++ Day-4/Datasource.tf
@@ -1,27 +1,33 @@
 data "aws_ami" "amzlinux" {
+
   most_recent = true
-  owners = [ "amazon" ]
+  owners      = ["amazon"]
+
   filter {
-    name = "name"
-    values = [ "amzn2-ami-hvm-*-gp2" ]
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-gp2"]
   }
+
   filter {
-    name = "root-device-type"
-    values = [ "ebs" ]
+    name   = "root-device-type"
+    values = ["ebs"]
   }
+
   filter {
-    name = "virtualization-type"
-    values = [ "hvm" ]
+    name   = "virtualization-type"
+    values = ["hvm"]
   }
+
   filter {
-    name = "architecture"
-    values = [ "x86_64" ]
+    name   = "architecture"
+    values = ["x86_64"]
   }
+
 }
 
 
 resource "aws_instance" "test" {
-    ami = data.aws_ami.amzlinux.id
-    instance_type = "t2.micro"
-  
+  ami           = data.aws_ami.amzlinux.id
+  instance_type = "t2.micro"
+
 }
