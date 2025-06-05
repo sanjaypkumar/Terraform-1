8c2eb724b9c11299a4f4626800df7bf7569637b7
sanjayyadav <palleboinasanjaykumar@gmail.com>
2025-06-05 07:46:47 +0530

    ec2 file updated

--- Day-5/ec2.tf
+++ Day-5/ec2.tf
@@ -31,4 +31,25 @@ resource "aws_instance" "linux" {
   availability_zone           = "ap-south-1a"
   security_groups             = [aws_security_group.dev_sg.id]
   user_data                   = file("httpd.sh")
+  depends_on                  = [aws_ebs_volume.dev]
+}
+
+# creating ebs volume in the same availabilty zone where as ec2 instance created
+
+resource "aws_ebs_volume" "dev" {
+
+  availability_zone = "ap-south-1a"
+  size              = 8
+  tags = {
+    Name = "dev_ebs_additional_volume"
+  }
+}
+
+# attach the ebs volume to the ec2 instance 
+
+resource "aws_volume_attachment" "ebs_att" {
+  device_name = "/dev/sdh"
+  volume_id   = aws_ebs_volume.dev.id
+  instance_id = aws_instance.linux.id
+
 }
