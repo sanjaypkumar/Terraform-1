82e55f4ab752dc90fb6619907fe071ec791ba6d5
sanjayyadav <palleboinasanjaykumar@gmail.com>
2025-06-08 20:38:25 +0530

    update module

---
+++ Day-7-module/root/variable.tf
@@ -0,0 +1,16 @@
+variable "ami_id" {
+  type = string
+  default = ""
+}
+
+variable "instance_type" {
+    type = string
+    default = ""
+  
+}
+
+variable "availability_zone" {
+    type = string
+    default = ""
+  
+}
