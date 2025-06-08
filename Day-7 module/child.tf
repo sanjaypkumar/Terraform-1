02b8742f9630b7c1447928b1a30032b4455dc98d
sanjaykumar <102154219+sanjaypkumar@users.noreply.github.com>
2025-06-08 20:39:58 +0530

    Delete Day-7 module directory

--- Day-7 module/child.tf
+++
@@ -1,8 +0,0 @@
-module "ec2_child" {
-  source = "./root"
-  ami_id = "ami-0f535a71b34f2d44a" # Example AMI ID, replace with actual
-  # This AMI ID is for Amazon Linux 2 in the us-east-1 region.
-  instance_type     = "t3.micro"
-  availability_zone = "ap-south-1b" # Example availability zone, replace with actual
-
-}
--- Day-7 module/root/terraform.tfvars
+++
@@ -1,4 +0,0 @@
-ami_id = "ami-0f535a71b34f2d44a" # Replace with the actual AMI ID you want to use
-# This AMI ID is for Amazon Linux 2 in the ap-south-1 region (Mumbai).
-instance_type     = "t2.micro"
-availability_zone = "ap-south-1a"
